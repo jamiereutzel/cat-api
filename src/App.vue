6d0c1287efc90597ef864c505d9aff5fe97132a4
Jamie <jamier@o3world.com>
2019-12-05 22:42:47 -0500

    Create Hero Component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <hero/>
     <search/>
   </div>
 </template>
 
 <script>
+import Hero from './components/Hero'
 import Search from './components/Search'
 
 export default {
   name: 'app',
   components: {
-    Search
+    Search,
+    Hero
   }
 }
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hero">
+    <h1>This is the title</h1>
+    <p>This is the subtitle</p>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Hero',
+    data () {
+      return { }
+    }
+  }
+</script>
