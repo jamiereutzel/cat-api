1df3b4bec653725bd09f9b5c2e61267bb080d615
Jamie <jamier@o3world.com>
2019-12-05 21:00:28 -0500

    Add Basic Button With Output When Clicked

---
+++ src/components/Search.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="search">
+    <button v-on:click="buttonClicked">Click Me</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Search',
+    data () {
+      return { }
+    },
+    methods: {
+      buttonClicked: function (event) {
+        console.log("Button Has Been Clicked");
+      }
+    }
+  }
+</script>
