35ccff5ed54b458bab4793b7a32ce2ddd7b0eeb1
Jamie <jamier@o3world.com>
2019-12-05 23:26:10 -0500

    Update Content

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hero">
-    <h1>This is the title</h1>
-    <p>This is the subtitle</p>
+    <h1>URBN Code Test</h1>
+    <p>Hello! Welcome to Jamie's Code Test. This simple app uses Vue.js to pull pictures from <a href="https://thecatapi.com/" target="_blank">thecatapi!</a></p>
   </div>
 </template>
 
