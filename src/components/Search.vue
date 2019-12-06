fc879ae9a21ba22376b057598cc967cafb73c992
Jamie <jamier@o3world.com>
2019-12-05 23:50:39 -0500

    Refactor Image Grid

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,8 +3,10 @@
     <div class="search__container">
       <button v-on:click="buttonClicked" class="btn">Click Me</button>
 
-      <div v-if="result" class="img-container">
-        <img v-bind:src="result" class="img" />
+      <div v-if="results" class="img-grid">
+        <div v-for="result in results" class="img-grid__item">
+          <img v-bind:src="result.url" class="img-grid__img" />
+        </div>
       </div>
     </div>
   </div>
@@ -18,15 +20,15 @@
     name: 'Search',
     data () {
       return {
-        result: ''
+        results: ''
       }
     },
     methods: {
       buttonClicked: function (event) {
         axios.defaults.headers.common['x-api-key'] = "59d8c29e-53dc-4dde-93f3-a5aea8aaa5a9" 
-        axios.get('https://api.thecatapi.com/v1/images/search').then( response => {
+        axios.get('https://api.thecatapi.com/v1/images/search?limit=10').then( response => {
           console.log(response.data);
-          this.result = response.data[0].url;
+          this.results = response.data;
         });
       }
     }
