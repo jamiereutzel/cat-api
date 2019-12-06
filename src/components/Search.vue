ea09e97136617d5e710e01320b1b13ca0dd228cb
Jamie <jamier@o3world.com>
2019-12-06 15:37:45 -0500

    Create Queries for Selecting Category and Breeds

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -7,9 +7,9 @@
         <label for="" class="select-menu__label">Category:</label>
         <div class="select-menu__wrapper">
           <img src="../assets/arrow.png" class="select-menu__icon" />
-          <select class="select-menu__select">
+          <select class="select-menu__select" @change="onCategoryChange($event)">
             <option value="" disabled selected hidden>Select</option>
-            <option v-for="category in categories" value="category.id">{{category.name}}</option>
+            <option v-for="category in categories" v-bind:value="category.id">{{category.name}}</option>
           </select>
         </div>
       </div>
@@ -18,9 +18,9 @@
         <label for="" class="select-menu__label">Breeds:</label>
         <div class="select-menu__wrapper">
           <img src="../assets/arrow.png" class="select-menu__icon" />
-          <select class="select-menu__select">
+          <select class="select-menu__select" @change="onBreedChange($event)">
             <option value="" disabled selected hidden>Select</option>
-            <option v-for="breed in breeds" value="breed.id">{{breed.name}}</option>
+            <option v-for="breed in breeds" v-bind:value="breed.id">{{breed.name}}</option>
           </select>
         </div>
       </div>
@@ -30,6 +30,7 @@
           <img v-bind:src="result.url" class="img-grid__img" />
         </div>
       </div>
+
     </div>
   </div>
 
@@ -55,25 +56,34 @@
 
       getCategories() {
         axios.get('https://api.thecatapi.com/v1/categories').then((response) => {
-          console.log(response.data);
           this.categories = response.data;
         }).catch( error => { console.log(error); });
       },
 
       getBreeds() {
         axios.get('https://api.thecatapi.com/v1/breeds').then((response) => {
-          console.log(response.data);
           this.breeds = response.data;
         }).catch( error => { console.log(error); });
       },
 
       buttonClicked: function (event) {
-        axios.defaults.headers.common['x-api-key'] = "59d8c29e-53dc-4dde-93f3-a5aea8aaa5a9";
-        axios.get('https://api.thecatapi.com/v1/images/search?limit=10').then( response => {
-          console.log(response.data);
+        axios.get('https://api.thecatapi.com/v1/images/search?limit=15').then( response => {
+          this.results = response.data;
+        });
+      },
+
+      onCategoryChange(event) {
+        axios.get('https://api.thecatapi.com/v1/images/search?limit=15&category_ids='+event.target.value).then( response => {
+          this.results = response.data;
+        });
+      },
+
+      onBreedChange(event) {
+        axios.get('https://api.thecatapi.com/v1/images/search?limit=15&breed_id='+event.target.value).then( response => {
           this.results = response.data;
         });
       }
+
     }
   }
 </script>
