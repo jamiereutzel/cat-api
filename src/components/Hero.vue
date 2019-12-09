f7b36dd5998740f081e9dccd3199f0a405138a2c
Jamie <jamier@o3world.com>
2019-12-09 09:47:40 -0500

    Update Content

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,7 +5,7 @@
     <h2>Description:</h2>
     <p>This simple app uses Vue.js to pull pictures from <a href="https://thecatapi.com/" target="_blank">thecatapi!</a></p>
     <h2>Usage:</h2>
-    <p>On page load, the API displays the first 15 images. The select dropdowns are used to filter results by cateogory or breed.</p>
+    <p>On page load, twelve random images from the API are displayed. The select dropdowns are used to filter results by cateogory or breed.</p>
   </div>
 </template>
 
